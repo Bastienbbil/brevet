d2661c3ac59b647a169749f8752d889ad8a04d25
Lucy Liu <jliu176@gmail.com>
2020-03-03 14:51:31 +0100

    amend to sklearn pipeline wkfl

--- problem.py
+++ problem.py
@@ -2,27 +2,19 @@ import os
 import numpy as np
 import pandas as pd
 import rampwf as rw
-from rampwf.workflows import FeatureExtractorRegressor
 from rampwf.score_types.base import BaseScoreType
 from sklearn.model_selection import ShuffleSplit
 from sklearn.metrics import mean_squared_error
 
 
-problem_title = 'Prediction of succes rates at the Brevet exam'
-_target_column_name = 'target' 
+problem_title = 'Prediction of success rates at the Brevet exam'
+_target_column_name = 'target'
 # A type (class) which will be used to create wrapper objects for y_pred
 Predictions = rw.prediction_types.make_regression()
 # An object implementing the workflow
+workflow = rw.workflows.EstimatorExternalData()
 
-class BREVET(FeatureExtractorRegressor):
-    def __init__(self, workflow_element_names=[
-            'feature_extractor', 'regressor', 'cities_data_filtered.csv']):
-        super(BREVET, self).__init__(workflow_element_names[:2])
-        self.element_names = workflow_element_names
-
-workflow = BREVET()
-
-# define the score 
+# define the score
 class NormalizedRMSE(BaseScoreType):
     is_lower_the_better = True
     minimum = 0.0
@@ -34,8 +26,8 @@ class NormalizedRMSE(BaseScoreType):
 
     def __call__(self, y_true, y_pred):
         rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-        return rmse / np.std(y_true) 
-    
+        return rmse / np.std(y_true)
+
 score_types = [
     NormalizedRMSE(name='normalized rmse', precision=5),
 ]
---
+++ submissions/starting_kit/estimator.py
@@ -0,0 +1,123 @@
+import os
+
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder, StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.base import BaseEstimator
+
+
+def _process_students(X):
+    """Create new features linked to the pupils"""
+
+    # average class size
+    X['average_class_size'] = X['Nb élèves'] / X['Nb divisions']
+    # percentage of pupils in the general stream
+    X['percent_general_stream'] = X['Nb 6èmes 5èmes 4èmes et 3èmes générales'] / X['Nb élèves']
+    # percentage of pupils in an european or international section
+    X['percent_euro_int_section'] = X['Nb 6èmes 5èmes 4èmes et 3èmes générales sections européennes et internationales'] / X['Nb élèves']
+    # percentage of pupils doing Latin or Greek
+    sum_global_5_to_3 = X['Nb 5èmes'] + X['Nb 4èmes générales'] + X['Nb 3èmes générales']
+    X['percent_latin_greek'] = X['Nb 5èmes 4èmes et 3èmes générales Latin ou Grec'] / sum_global_5_to_3
+    # percentage of pupils that are in a SEGPA class
+    X['percent_segpa'] = X['Nb SEGPA'] / X['Nb élèves']
+
+    return np.c_[X['average_class_size'].values,
+                    X['percent_general_stream'].values,
+                    X['percent_euro_int_section'].values,
+                    X['percent_latin_greek'].values,
+                    X['percent_segpa'].values]
+
+def _merge_naive(X):
+
+    # read the database with the city information
+    filepath = os.path.join(
+        os.path.dirname(__file__), 'external_data.csv'
+    )
+    cities_data = pd.read_csv(filepath, index_col=0)
+    # merge the two databases at the city level
+    df = pd.merge(
+        X, cities_data,left_on='Commune et arrondissement code',
+        right_on='insee_code', how='left'
+    )
+    keep_col_cities = [
+        'population',
+        'SUPERF',
+        'med_std_living',
+        'poverty_rate',
+        'unemployment_rate'
+    ]
+    # fill na by taking the average value at the departement level
+    for col in keep_col_cities:
+        if cities_data[col].isna().sum() > 0:
+            df[col] = df[['Département code', col]].groupby('Département code').transform(lambda x: x.fillna(x.mean()))
+
+    return df[keep_col_cities]
+
+def get_estimator():
+
+    students_col = [
+        'Nb élèves', 'Nb divisions', 'Nb 6èmes 5èmes 4èmes et 3èmes générales',
+        'Nb 6èmes 5èmes 4èmes et 3èmes générales sections européennes et internationales',
+        'Nb 5èmes', 'Nb 4èmes générales', 'Nb 3èmes générales',
+        'Nb 5èmes 4èmes et 3èmes générales Latin ou Grec', 'Nb SEGPA'
+    ]
+    num_cols = [
+        'Nb élèves', 'Nb 3èmes générales', 'Nb 3èmes générales retardataires',
+        "Nb 6èmes provenant d'une école EP"
+    ]
+    cat_cols = [
+        'Appartenance EP', 'Etablissement sensible', 'CATAEU2010',
+        'Situation relative à une zone rurale ou autre'
+    ]
+    merge_col = [
+        'Commune et arrondissement code', 'Département code'
+    ]
+    drop_cols = [
+        'Name', 'Coordonnée X', 'Coordonnée Y', 'Commune code', 'City_name',
+        'Commune et arrondissement code', 'Commune et arrondissement nom',
+        'Département nom', 'Académie nom', 'Région nom', 'Région 2016 nom',
+        'Longitude', 'Latitude', 'Position'
+    ]
+
+    numeric_transformer = Pipeline(steps=[
+        ('scale', StandardScaler())
+    ])
+    categorical_transformer = Pipeline(steps=[
+        ('encode', OneHotEncoder(handle_unknown='ignore'))
+    ])
+    students_transformer = FunctionTransformer(
+        _process_students, validate=False
+    )
+    students_transformer = make_pipeline(
+                students_transformer, SimpleImputer(strategy='mean'),
+                StandardScaler()
+            )
+    merge_transformer = FunctionTransformer(_merge_naive, validate=False)
+    merge_transformer = make_pipeline(
+        merge_transformer, SimpleImputer(strategy='mean')
+    )
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numeric_transformer, num_cols),
+            ('cat', categorical_transformer, cat_cols),
+            ('students', students_transformer, students_col),
+            ('merge', merge_transformer, merge_col),
+            ('drop cols', 'drop', drop_cols),
+        ], remainder='passthrough') # remainder='drop' or 'passthrough'
+
+    regressor = RandomForestRegressor(
+            n_estimators=5, max_depth=50, max_features=10
+        )
+
+    pipeline = Pipeline(steps=[
+        ('preprocessing', preprocessor),
+        ('classifier', regressor)
+    ])
+
+    return pipeline
