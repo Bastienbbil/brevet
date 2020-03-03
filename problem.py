b6d9e3f0dee61754454ef6ee3809b0c9e1dc3d94
Lucy Liu <jliu176@gmail.com>
2020-03-03 16:34:46 +0100

    flake8

--- problem.py
+++ problem.py
@@ -1,4 +1,5 @@
 import os
+
 import numpy as np
 import pandas as pd
 import rampwf as rw
@@ -14,7 +15,7 @@ Predictions = rw.prediction_types.make_regression()
 # An object implementing the workflow
 workflow = rw.workflows.EstimatorExternalData()
 
-# define the score
+
 class NormalizedRMSE(BaseScoreType):
     is_lower_the_better = True
     minimum = 0.0
@@ -28,13 +29,16 @@ class NormalizedRMSE(BaseScoreType):
         rmse = np.sqrt(mean_squared_error(y_true, y_pred))
         return rmse / np.std(y_true)
 
+
 score_types = [
     NormalizedRMSE(name='normalized rmse', precision=5),
 ]
 
+
 def get_cv(X, y):
     cv = ShuffleSplit(n_splits=8, test_size=0.20, random_state=42)
     return cv.split(X, y)
+
 
 def _read_data(path, f_name):
     data = pd.read_csv(os.path.join(path, 'data', f_name), index_col=0)
@@ -42,9 +46,11 @@ def _read_data(path, f_name):
     X_df = data.drop(_target_column_name, axis=1)
     return X_df, y_array
 
+
 def get_train_data(path='.'):
     f_name = 'data_college_filtered_TRAIN.csv'
     return _read_data(path, f_name)
+
 
 def get_test_data(path='.'):
     f_name = 'data_college_filtered_TEST.csv'
--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -5,10 +5,10 @@ import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder, StandardScaler, OneHotEncoder
+from sklearn.preprocessing import FunctionTransformer, StandardScaler, \
+    OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.base import BaseEstimator
 
 
 def _process_students(X):
@@ -17,20 +17,27 @@ def _process_students(X):
     # average class size
     X['average_class_size'] = X['Nb élèves'] / X['Nb divisions']
     # percentage of pupils in the general stream
-    X['percent_general_stream'] = X['Nb 6èmes 5èmes 4èmes et 3èmes générales'] / X['Nb élèves']
+    X['percent_general_stream'] = \
+        X['Nb 6èmes 5èmes 4èmes et 3èmes générales'] / X['Nb élèves']
     # percentage of pupils in an european or international section
-    X['percent_euro_int_section'] = X['Nb 6èmes 5èmes 4èmes et 3èmes générales sections européennes et internationales'] / X['Nb élèves']
+    X['percent_euro_int_section'] = \
+        X['Nb 6èmes 5èmes 4èmes et 3èmes générales sections européennes et internationales'] / X['Nb élèves'] # noqa
     # percentage of pupils doing Latin or Greek
-    sum_global_5_to_3 = X['Nb 5èmes'] + X['Nb 4èmes générales'] + X['Nb 3èmes générales']
-    X['percent_latin_greek'] = X['Nb 5èmes 4èmes et 3èmes générales Latin ou Grec'] / sum_global_5_to_3
+    sum_global_5_to_3 = \
+        X['Nb 5èmes'] + X['Nb 4èmes générales'] + X['Nb 3èmes générales']
+    X['percent_latin_greek'] = \
+        X['Nb 5èmes 4èmes et 3èmes générales Latin ou Grec'] / sum_global_5_to_3 # noqa
     # percentage of pupils that are in a SEGPA class
     X['percent_segpa'] = X['Nb SEGPA'] / X['Nb élèves']
 
-    return np.c_[X['average_class_size'].values,
-                    X['percent_general_stream'].values,
-                    X['percent_euro_int_section'].values,
-                    X['percent_latin_greek'].values,
-                    X['percent_segpa'].values]
+    return np.c_[
+        X['average_class_size'].values,
+        X['percent_general_stream'].values,
+        X['percent_euro_int_section'].values,
+        X['percent_latin_greek'].values,
+        X['percent_segpa'].values
+    ]
+
 
 def _merge_naive(X):
 
@@ -41,28 +48,28 @@ def _merge_naive(X):
     cities_data = pd.read_csv(filepath, index_col=0)
     # merge the two databases at the city level
     df = pd.merge(
-        X, cities_data,left_on='Commune et arrondissement code',
+        X, cities_data, left_on='Commune et arrondissement code',
         right_on='insee_code', how='left'
     )
     keep_col_cities = [
-        'population',
-        'SUPERF',
-        'med_std_living',
-        'poverty_rate',
+        'population', 'SUPERF', 'med_std_living', 'poverty_rate',
         'unemployment_rate'
     ]
     # fill na by taking the average value at the departement level
     for col in keep_col_cities:
         if cities_data[col].isna().sum() > 0:
-            df[col] = df[['Département code', col]].groupby('Département code').transform(lambda x: x.fillna(x.mean()))
+            df[col] = df[['Département code', col]]. \
+                groupby('Département code'). \
+                transform(lambda x: x.fillna(x.mean()))
 
     return df[keep_col_cities]
+
 
 def get_estimator():
 
     students_col = [
         'Nb élèves', 'Nb divisions', 'Nb 6èmes 5èmes 4èmes et 3èmes générales',
-        'Nb 6èmes 5èmes 4èmes et 3èmes générales sections européennes et internationales',
+        'Nb 6èmes 5èmes 4èmes et 3èmes générales sections européennes et internationales', # noqa
         'Nb 5èmes', 'Nb 4èmes générales', 'Nb 3èmes générales',
         'Nb 5èmes 4èmes et 3èmes générales Latin ou Grec', 'Nb SEGPA'
     ]
@@ -109,7 +116,7 @@ def get_estimator():
             ('students', students_transformer, students_col),
             ('merge', merge_transformer, merge_col),
             ('drop cols', 'drop', drop_cols),
-        ], remainder='passthrough') # remainder='drop' or 'passthrough'
+        ], remainder='passthrough')  # remainder='drop' or 'passthrough'
 
     regressor = RandomForestRegressor(
             n_estimators=5, max_depth=50, max_features=10
